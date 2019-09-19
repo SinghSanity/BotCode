17e7a7219ab758df8564d5c59329cca3dfc4b1fe
Amandeep Singh <asinghsanity@gmail.com>
2019-09-19 16:17:20 -0400

    Prefix commands fix. Temp loss of Clear command.

--- index.js
+++ index.js
@@ -2,24 +2,25 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-//Importing Token from my private token.js file
-var myModule = require('./token');
-var token = myModule.token;
+//Importing Token and Prefix from my private startup.js file
+var myModule = require('./startup.js');
 
-//Discord bot Prefix
-const Prefix = '!';
+var token = myModule.token; // Discord bot Token
+
+var prefix = myModule.prefix; // Discord bot Prefix
 
 //Log message when it turns on successfully. 
+
 bot.on('ready', () => {
     console.log('This bot is online!');
 })
 
 // Text Stuff 1
 // Will move this on to another file in the future
-
+/*
 bot.on('message', message => {
 
-    let args = message.content.substring(Prefix.length).split(" ");
+    let args = message.content.substring(prefix.length).split(" ");
     switch (args[0]) {
         //Basic commands
         case 'ping':
@@ -45,6 +46,35 @@ bot.on('message', message => {
 
     }
 })
+*/
+
+bot.on('message', message => {
+
+    if (message.content === prefix + 'ping') {
+        message.channel.sendMessage('pong!');
+    }
+    if (message.content === prefix + 'pong') {
+        message.channel.sendMessage('ping!');
+    }
+    if (message.content === prefix + 'version') {
+        message.channel.sendMessage('I am currently on Beta 1.0.0');
+    }
+    if (message.content === prefix + 'help') {
+        message.channel.sendMessage('I also need help, but no one ever does');
+    }
+    if (message.content === prefix + 'server') {
+        message.channel.sendMessage("This server's name is " + message.guild.name +
+            ". There are " + message.guild.memberCount + " people here.");
+    }
+    if (message.content === prefix + 'clear') {
+
+
+
+    }
+})
+
+
+
 
 bot.on('message', msg => {
     if (msg.content === "HELLO") {
@@ -62,6 +92,7 @@ bot.on('message', msg => {
     if (msg.content === "hello" || msg.content === "Hello") {
         msg.reply("hello");
     }
+
 
 
 })
